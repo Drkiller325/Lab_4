35c3b7964ebff8cf2fa37bdeb425182c0bbcfb27
Drkiller325 <ahmed-2008-04@hotmail.com>
2022-12-06 16:09:09 +0200

    first Commit

---
+++ test/test/Classes/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.Classes
+{
+     public class Person
+    {
+        public string name { get; set; }
+        public decimal balance { get; set; }
+
+        private Random rand = new Random();
+
+        public Person()
+        {
+            name = Person.GenerateName(5);
+            balance = rand.Next(1000, 5000);
+        }
+
+        public static string GenerateName(int len)
+        {
+            Random r = new Random();
+            string[] consonants = { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "l", "n", "p", "q", "r", "s", "sh", "zh", "t", "v", "w", "x" };
+            string[] vowels = { "a", "e", "i", "o", "u", "ae", "y" };
+            string Name = "";
+            Name += consonants[r.Next(consonants.Length)].ToUpper();
+            Name += vowels[r.Next(vowels.Length)];
+            int b = 2; 
+            while (b < len)
+            {
+                Name += consonants[r.Next(consonants.Length)];
+                b++;
+                Name += vowels[r.Next(vowels.Length)];
+                b++;
+            }
+
+            return Name;
+
+
+        }
+    }
+}
